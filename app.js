89359ac5cc767f54ee714fadacea55f615fa93f3
Josh Viktorov <joshviktorov27@gmail.com>
2020-07-08 13:57:02 -0400

    hooked up api

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +18,8 @@ app.use(cookieSession({
     maxAge: 24*60*60*1000,
     keys: [keys.session.cookieKey]
 }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -34,6 +37,33 @@ app.get('/', (req, res) => {
         user: req.user
     });
 });
+
+app.get('/movies', (req, res) => {
+    res.render('movies', {
+        user: req.user,
+        movies: []
+    })
+});
+
+app.post('/movies', (req, res) => {
+    
+    let search = encodeURI(req.body.search);
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=ed07fde2a29e865fa73860b991476f93&query=' + search + '&page=1').then((data) => {
+        
+        res.render('movies', {
+            user: req.user,
+            movies: data.data.results
+        });
+
+    });
+});
+
+app.post('/movies/add', (req, res) => {
+    let id = req.body.id;
+
+    res.redirect('/movies');
+});
+
 
 app.listen(3000, () => {
     console.log("Server running at port 3000");
